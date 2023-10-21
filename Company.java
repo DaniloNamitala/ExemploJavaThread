e5118c2dc0ab249e2993cfb4c0cf2effc31477d2
Danilo Namitala <danilonamitala@yahoo.com.br>
2023-10-20 21:40:42 -0300

    Primeiro commit

---
+++ Company.java
@@ -0,0 +1,29 @@
+import java.net.Socket;
+
+public class Company extends Service {
+
+    Company(int port) {
+        super(port);
+    }
+
+    public int calcPreco(int peso, int distancia) {
+        return peso * distancia;
+    }
+
+    @Override
+    public Server CreateServerThread(Socket conn) {
+        return new CompanyServer(conn);
+    }
+
+    public class CompanyServer extends Server {
+        public CompanyServer(Socket conn) {
+            super(conn);
+        }
+
+        @Override
+        protected void ProcessMessage(String message) {
+            System.out.println("[company] received: " + message);
+            SendMessage("MESSAGE RECEIVED");
+        }
+    }
+}
